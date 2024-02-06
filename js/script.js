a8cf160dd3bf5be76f8e2b738bbeebe58ea1831b
erkin7333 <erkinnajimov90@gmail.com>
2024-02-06 12:45:24 +0500

    Praktika

--- js/script.js
+++ js/script.js
@@ -3,11 +3,28 @@ let designBtn = document.getElementById('designbtn')
 let blogBtn = document.getElementById('blogbtn')
 let aboutBtn = document.getElementById('aboutbtn')
 let text = document.getElementsByTagName('span')[0]
+let textBtna = document.getElementsByTagName('a')[0]
 console.log(text)
+console.log(textBtna)
 let searchBtn = document.getElementsByClassName('search-btn')
 console.log(searchBtn[0])
 
-
+// Javascriptda elemetdagi kontentlani uzgartirish birinchi usuli
 function hover() {
-    text.textContent = 'Java'
+    textBtna.textContent = "Flutter"
 }
+
+// Javascriptda elemetdagi kontentlani uzgartirish ikkinchi usuli
+
+// text.onmouseenter = hover
+// textBtna.onmouseenter = hover
+
+// Javascriptda elemetdagi kontentlani uzgartirish uchinchi usuli
+//  Eng qulay usuli shu
+
+text.addEventListener('mouseenter', function () {
+    text.textContent = 'Java'
+})
+text.addEventListener('mouseleave', function () {
+    text.textContent = 'Python'
+})

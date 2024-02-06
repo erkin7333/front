5c9f5499aa2d9247ef9dd72f3235c0fdb5331442
erkin7333 <erkinnajimov90@gmail.com>
2024-02-06 11:59:28 +0500

    Python va Java

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 let homeBtn = document.getElementById('homebtn')
-console.log(homeBtn)
+let designBtn = document.getElementById('designbtn')
+let blogBtn = document.getElementById('blogbtn')
+let aboutBtn = document.getElementById('aboutbtn')
+let text = document.getElementsByTagName('span')[0]
+console.log(text)
+let searchBtn = document.getElementsByClassName('search-btn')
+console.log(searchBtn[0])
+
+
+function hover() {
+    text.textContent = 'Java'
+}
